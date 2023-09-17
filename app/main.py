3918077b55edaf44627c8fb249f53252a1b0c67b
Raushan Raj <90108340+raushanraj9427@users.noreply.github.com>
2023-09-17 12:13:29 +0530

    add post request to create plant

--- app/main.py
+++ app/main.py
@@ -56,6 +56,16 @@ async def read_detail(plant_id: int, db: Session = Depends(get_db)):
 
         raise HTTPException(status_code=404, detail='Details not found')
 
+@app.post("/plants/")
+async def create(plant: PlantBase, db: Session = Depends(get_db)):
+    db_plant = models.Plants(plant_text = plant.plant_text)
+    db.add(db_plant)
+    db.commit()
+    db.refresh(db_plant)
+    for choice in plant.choices:
+        db_choice = models.Details(plant_family=choice.plant_family, plant_bio=choice.plant_bio, plant_descr=choice.plant_descr, plant_url= choice.plant_url, plant_id=db_plant.id)
+        db.add(db_choice)
+    db.commit()
 
 
 
